258a5e277f022d4866048b6ffdd9c34663c005d1
Tioluwani <codemeastro101@gmail.com>
2024-08-01 14:42:22 +0100

    changed backend files for better connectivity

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,24 +1,10 @@
 import mongoose from "mongoose";
 
-let isConnected = false;
-
 export async function connectToMongoDB() {
-  if (isConnected) {
-    console.log('Using existing database connection');
-    return;
-  }
-
-  if (!process.env.URL) {
-    throw new Error('MongoDB URL is not defined');
-  }
-
   try {
-    const db = await mongoose.connect(process.env.URL);
-
-    isConnected = true;
-    console.log(`MongoDB Connected: ${db.connection.host}`);
+    await mongoose.connect(process.env.URL);
+    console.log("Connected to Database");
   } catch (error) {
-    console.error("Error connecting to Database:", error);
-    throw error; // re-throw the error for the caller to handle
+    console.error("Error connecting to Database", error);
   }
 }
