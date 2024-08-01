fb6689a7b68ff5f67b1cef00f306911b9446546b
Tioluwani <codemeastro101@gmail.com>
2024-08-01 13:35:44 +0100

    worked on the mongodb settimeout

--- src/app/api/todos/route.js
+++ src/app/api/todos/route.js
@@ -8,6 +8,6 @@ export async function GET(req) {
     return NextResponse.json({ status: 201, data: allTodos });
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ status: 500, message: "Internal Server Error" });
+    return NextResponse.json({ status: 500, message: "Internal Server Error", error });
   }
 }
