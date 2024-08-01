147c1eca7985f045db1bb3b48efc2f67c9cd6217
Tioluwani <codemeastro101@gmail.com>
2024-08-01 13:55:09 +0100

    worked on the mongodb set timeout fixed 1st issue

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 export async function connectToMongoDB() {
   try {
-    await mongoose.connect(process.env.URL, {
-      serverSelectionTimeoutMS: 30000,
-      socketTimeoutMS: 45000,
-    });
+    await mongoose.connect(process.env.URL);
     console.log("Connected to Database");
   } catch (error) {
     console.error("Error connecting to Database", error);
