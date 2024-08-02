fb5b7c13c52c4fd764e9879bf0fa0831206a8e9b
Tioluwani <codemeastro101@gmail.com>
2024-08-02 11:31:47 +0100

    changed the .env.local file

--- src/app/page.js
+++ src/app/page.js
@@ -21,6 +21,7 @@ export default function Home() {
 
     if (response.status === 201) {
       showToast("Todo has been added successfully");
+      console.log(response.status)
     }
   }
 
--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connectToMongoDB() {
   try {
-    await mongoose.connect(process.env.URL);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to Database");
   } catch (error) {
     console.error("Error connecting to Database", error);
